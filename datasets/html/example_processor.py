b93b4a138f5f51f176d620920831a0cfcf6bdfb5
Scott Antipa <scottantipa@gmail.com>
2020-08-16 20:10:27 -0700

    properly parse utterance before pasing to standalone parser

--- datasets/html/example_processor.py
+++ datasets/html/example_processor.py
@@ -1,5 +1,6 @@
 from common.registerable import Registrable
 from datasets.utils import ExampleProcessor
+from datasets.html.dataset import tokenize_english_utterance
 
 @Registrable.register('html_example_processor')
 class HtmlExampleProcessor(ExampleProcessor):
@@ -7,7 +8,7 @@ class HtmlExampleProcessor(ExampleProcessor):
         self.transition_system = transition_system
 
     def pre_process_utterance(self, utterance):
-        return utterance.split(' '), {}
+        return tokenize_english_utterance(utterance), {}
 
     def post_process_hypothesis(self, hyp, meta_info, utterance=None):
         code = self.transition_system.ast_to_surface_code(hyp.tree)
