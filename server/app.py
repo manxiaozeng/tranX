7cd737574ee0f5ff8202e5a1e9a53e45e024c3af
Scott Antipa <scottantipa@gmail.com>
2020-09-04 14:16:44 -0700

    add html config for server

--- server/app.py
+++ server/app.py
@@ -16,8 +16,6 @@ def init_arg_parser():
 
     #### General configuration ####
     arg_parser.add_argument('--cuda', action='store_true', default=False, help='Use gpu')
-    arg_parser.add_argument('--config_file', type=str, required=True,
-                            help='Config file that specifies model to load, see online doc for an example')
     arg_parser.add_argument('--port', type=int, required=False, default=8081)
 
     return arg_parser
@@ -64,7 +62,7 @@ def parse(utterance, dataset):
 
 if __name__ == '__main__':
     args = init_arg_parser().parse_args()
-    config_dict = json.load(open(args.config_file))
+    config_dict = json.load(open("server/html-config.json"))
 
     for parser_id, config in config_dict.items():
         parser = StandaloneParser(parser_name=config['parser'],
